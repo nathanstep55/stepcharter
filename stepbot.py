ffe08f937cdced0e7cb7224e774486e8482dcc9d
nathanstep55 <nathanstep55@gmail.com>
2018-08-19 08:17:53 -0400

    Implement algorithm and export functionality

--- stepbot.py
+++ stepbot.py
@@ -18,7 +18,7 @@ from builtins import input
 from io import open
 
 from random import randint, choice
-import sys
+import sys, codecs
 
 try: # Python 2
     from Tkinter import *
@@ -29,10 +29,11 @@ except ImportError: # Python 3
 
 # ~~~~~ Configure these settings! ~~~~~
 gamemode = "dance_single" # the only gamemode right now, will default if unknown
-crossover_between_measures = False # false to make an attempt at less awkward patterns
-random = False
+crossover_between_measures = False # doesn't work yet, but false to make an attempt at less awkward patterns
+random = False # completely random arrows, ignores patterns
 
 # Weights: Configure how often you want certain patterns to occur as a decimal
+# Note that if conditions are unmet for a pattern to occur, it will ignore the pattern (meaning it may happen less than expected)
 crossovers = 0.1
 spins = 0.05 # this doesn't necessarily mean a full spin
 footswitches = 0.05
@@ -78,37 +79,240 @@ def get_gm_num(gm): # how many arrows per gamemode
 
 def generate(note):
     new = []
+    L = 0
+    R = 3
     leftfoot = bool(randint(0,1))
-    lastnote = [0,0,0,0]
+    lastset2 = [0,0,0,1] # set this to randint choice
+    lastset = [1,0,0,0] # and make this the other one
     for a in range(len(note)):
         temp = []
+        fullholdlist = []
         for b in range(len(note[a])):
-            next = [0,1,2,3]
-            if note[a][b] = [0,0,0,0]:
+            if note[a][b] == [0,0,0,0]:
+                temp.append([0,0,0,0])
                 continue
-            if 2 in note[a][b]:
-                holds = [i for i, x in enumerate(note[a][b]) if x == 2]
-            if random:
-                n = randint(0,3)
-            else:
-                patterns = ['normal']*(100*normal) + ['crossovers']*(100*crossovers) + ['spins']*(100*spins) + ['footswitches']*(100*footswitches) + ['jacks']*(100*jacks) + ['repeats']*(100*repeats)
-                p = choice(patterns)
-                n = 0
-                if len(next) > 1:
-                    n = randint(0,len(next)-1)
-            if lastnote.count(1) + lastnote.count(2) > 1:
-                # this only handles one note i need to handle two notes
-                # probably random generation whether foot switches or not
-            temp.append() # note that it needs to be converted to the correct array
-            lastnote = next
-            leftfoot = not leftfoot
+            nextlist = []
+            holdlist = []
+            realleftfoot = leftfoot
+            for c in range(note[a][b].count('1')+note[a][b].count('2')): # gets all arrows
+                next = [0,1,2,3]
+                for d in nextlist:
+                    next.remove(d)
+                if random:
+                    n = randint(0,3)
+                if not random:
+                    patterns = ['normal']*int(100*normal) + ['crossovers']*int(100*crossovers) + ['spins']*int(100*spins) + ['footswitches']*int(100*footswitches) + ['jacks']*int(100*jacks) + ['repeats']*int(100*repeats)
+                    p = choice(patterns)
+                    if p == ['normal']: # to do: put all of these in functions to reduce redundant code
+                        if leftfoot: # 0,1,2
+                            if R == 0: # go back to last arrow
+                                lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                if [lastlist2[len(lastlist2)-1]] in nextlist:
+                                    next = [lastlist2[len(lastlist2)-1]]
+                                else:
+                                    next = [randint(0,3)]
+                            else:
+                                next.remove(3)
+                                next.remove(L)
+                                next.remove(R)
+                        else: # 1,2,3
+                            if L == 3:  # go back to last arrow
+                                lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                if [lastlist2[0]] in nextlist:
+                                    next = [lastlist2[0]]
+                                else:
+                                    next = [randint(0,3)]
+                            else:
+                                next.remove(0)
+                                next.remove(L)
+                                next.remove(R)
+                    elif p == ['crossovers']: # this will not start a crossover if it just happened (unless afronova walk)
+                        if L == 3 and R == 0:
+                            if leftfoot: # 0,1,2
+                                if R == 0: # go back to last arrow
+                                    lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                    if [lastlist2[len(lastlist2)-1]] in nextlist:
+                                        next = [lastlist2[len(lastlist2)-1]]
+                                    else:
+                                        next = [randint(0,3)]
+                                else:
+                                    next.remove(3)
+                                    next.remove(L)
+                                    next.remove(R)
+                            else: # 1,2,3
+                                if L == 3:  # go back to last arrow
+                                    lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                    if [lastlist2[0]] in nextlist:
+                                        next = [lastlist2[0]]
+                                    else:
+                                        next = [randint(0,3)]
+                                else:
+                                    next.remove(0)
+                                    next.remove(L)
+                                    next.remove(R)
+                        else: # idk if this works
+                            if leftfoot and R != 3:
+                                if 3 not in nextlist:
+                                    next = [3]
+                                else:
+                                    next = [randint(0,3)]
+                            elif not leftfoot and L != 0:
+                                if 0 not in nextlist:
+                                    next = [0]
+                                else:
+                                    next = [randint(0,3)]
+                            else:
+                                if leftfoot: # 0,1,2
+                                    if R == 0: # go back to last arrow
+                                        lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                        if [lastlist2[len(lastlist2)-1]] in nextlist:
+                                            next = [lastlist2[len(lastlist2)-1]]
+                                        else:
+                                            next = [randint(0,3)]
+                                    else:
+                                        next.remove(3)
+                                        next.remove(L)
+                                        next.remove(R)
+                                else: # 1,2,3
+                                    if L == 3:  # go back to last arrow
+                                        lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                        if [lastlist2[0]] in nextlist:
+                                            next = [lastlist2[0]]
+                                        else:
+                                            next = [randint(0,3)]
+                                    else:
+                                        next.remove(0)
+                                        next.remove(L)
+                                        next.remove(R)
+                    elif p == ['spins']: # not necessarily a spin but
+                        if leftfoot and R == 0:
+                            if L == 2:
+                                next = [1]
+                            elif L == 1:
+                                next = [2]
+                            else:
+                                next = [randint(1,3)]
+                        elif not leftfoot and L == 3:
+                            if R == 2:
+                                next = [1]
+                            elif R == 1:
+                                next = [2]
+                            else:
+                                next = [randint(1,3)]
+                        else:
+                            if leftfoot: # 0,1,2
+                                if R == 0: # go back to last arrow
+                                    lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                    if [lastlist2[len(lastlist2)-1]] in next:
+                                        next = [lastlist2[len(lastlist2)-1]]
+                                    else:
+                                        next = [randint(0,3)]
+                                else:
+                                    next.remove(3)
+                                    next.remove(L)
+                                    next.remove(R)
+                            else: # 1,2,3
+                                if L == 3:  # go back to last arrow
+                                    lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                    if [lastlist2[0]] in next:
+                                        next = [lastlist2[0]]
+                                    else:
+                                        next = [randint(0,3)]
+                                else:
+                                    next.remove(0)
+                                    next.remove(L)
+                                    next.remove(R)
+                    elif p == ['footswitches']:
+                        if leftfoot:
+                            if R == 2 or R == 1:
+                                next = [R]
+                            else:
+                                if R == 0: # go back to last arrow
+                                    lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                    next = [lastlist2[len(lastlist2)-1]]
+                                else:
+                                    next.remove(3)
+                                    next.remove(L)
+                                    next.remove(R)
+                        else:
+                            if L == 2 or L == 1:
+                                next = [L]
+                            else:
+                                if L == 3:  # go back to last arrow
+                                    lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                                    next = [lastlist2[0]]
+                                else:
+                                    next.remove(0)
+                                    next.remove(L)
+                                    next.remove(R)
+                    elif p == ['jacks']:
+                        lastlist = [i for i, x in enumerate(lastset) if x == 1 or x == 2]
+                        if leftfoot: # do left again
+                            next = [lastlist[0]]
+                        else: # right again
+                            next = [lastlist[len(lastlist)-1]]
+                    elif p == ['repeats']:
+                        lastlist2 = [i for i, x in enumerate(lastset2) if x == 1 or x == 2]
+                        if leftfoot: # do right
+                            next = [lastlist2[len(lastlist2)-1]]
+                        else: # left
+                            next = [lastlist2[0]]
+                    n = 0
+                    if len(next) > 1:
+                        n = randint(0,len(next)-1)
+                nextlist.append(next[n])
+                if leftfoot: # so this needs to be fixed plus same note cannot happen twice
+                    L = next[n]
+                else:
+                    R = next[n]
+                if p != ['jacks']:
+                    leftfoot = not leftfoot
+            for e in range(note[a][b].count('2')): # holds
+                holdlist.append(nextlist[e])
+                fullholdlist.append(nextlist[e])
+            for f in range(note[a][b].count('3')): # holdend
+                ends = [i for i, x in enumerate(note[a][b]) if x == 3] # since it's a string idk if it will work
+                for g in ends:
+                    fullholdlist.remove(note[a][b][g])
+            nextset = num_to_arr(nextlist, holdlist)
+            lastset2 = lastset
+            lastset = nextset
+            temp.append(nextset) # note that it needs to be converted to the correct array
+            leftfoot = not realleftfoot # multiple arrows it won't screw up step order
         new.append(temp)
-        
+    return new
+
+def num_to_arr(nextlist, holdlist):
+    array = [0,0,0,0]
+    for i in nextlist:
+        array[i] = 1
+    for j in holdlist:
+        array[j] = 2
+    return ''.join(str(e) for e in array)
+
+def export(sim, new, path):
+    e = codecs.open(path, 'w', encoding='UTF-8')
+    for i in range(len(sim)):
+        e.write(sim[i])
+        if "#NOTES:" in sim[i]:
+            for a in range(i,len(sim)):
+                if "     " not in sim[a]:
+                    break
+            for b in range(len(new)):
+                for c in range(len(new[b])):
+                    e.write(new[b][c]+"\n")
+                if b != len(new)-1:
+                    e.write(",\n")
+                else:
+                    e.write(";")
+            break
+    e.close()
+
 root = Tk()
 root.withdraw()
 path = filedialog.askopenfilename()
 
-f = open(path, 'rt')
+f = codecs.open(path, 'r', encoding='UTF-8')
 print("File opened.")
 
 simlines = f.readlines()
@@ -117,6 +321,10 @@ notes = get_notes(simlines) # convert notes split into arrays of measures split 
 print(notes)
 
 newchart = generate(notes)
+
+path2 = filedialog.asksaveasfilename()
+export(simlines, newchart, path2)
+print("Exported as simfile " + path2)
 
 # We're going to have to keep track of two different arrays:
 # - old notes to keep track of holds
